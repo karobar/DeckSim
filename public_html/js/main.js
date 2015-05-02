53e2c680d66f940c09bc4b58d9dd2f9ab413c520
RangDipkin <travisp471@gmail.com>
2015-05-01 22:04:27 -0400

    switched bootstrap version among many other changes

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -7,7 +7,8 @@ var crewCards;
 var tasks;
 var blairSaucer;
 var thousandMilesToCoast;
-var player1Hand, player2Hand;
+var player1Hand, player2Hand, thingHand;
+var allHands;
 
 //URL is a string
 //function loadFileAsString(URL) {
@@ -31,53 +32,89 @@ var player1Hand, player2Hand;
 
 $(document).ready(function() {
     console.log( "ready!" );
+    
+    $('#drawModal').on('show.bs.modal', function (event) { 
+        $('#cardIDinput').val('');
+        var invokingButton = $(event.relatedTarget);
+        var handTarget = invokingButton.attr('data-hand');
+        
+        $("#deckRadios").empty();
+        for(var i=0; i < allDecks.length; i++) {
+            //console.log("allDecks[" + i + "] = " +allDecks[i].name);
+            if(i === 0) {
+                $("#deckRadios").append("<label class='btn btn-default active'><input type='radio' name='options' data-cheating='"+allDecks[i].name+"' checked>"+ allDecks[i].name + "</label>");
+            }
+            else {    
+                $("#deckRadios").append("<label class='btn btn-default'><input type='radio' name='options' data-cheating='"+allDecks[i].name+"'>"+ allDecks[i].name + "</label>");
+            }
+        }
+        
+        //The unbind-then-bind trick was used because this listener was
+        //executing twice for whatever reason
+        $('#removeCardAcceptButton').unbind('click').bind('click', function () {
+            //console.log("remove card button pressed!");
+            var cardID = $("#cardIDinput").val();
+            var deckString = $("#deckRadios input:radio:checked").attr('data-cheating');
+            //console.log("getting deck from name: " + deckString);
+            var deckObj = getDeckFromName(deckString);
+            //console.log("handTarget: " + handTarget);
+            var handObj = getHandFromHTMLid(handTarget);
+            //console.log("deckObj: " + deckObj);
+            handObj.drawSpecificCard(deckObj, cardID);
+        });
+    });
+    
+    $('#chooseCharModal').on('show.bs.modal', function (e) {
+        for(var i=0; i < crewCards.length(); i++) {
+            $("#chooseCharacterBody").append("<button style='margin:7px 15px 17px 0;' type='button' class='btn btn-success' data-toggle='popover' data-trigger='hover' data-placement='top' data-content='"+crewCards.get(i).desc+"'>"+crewCards.get(i).name+"</button>");
+        }
+        var theModal = $(this);
+        var invokingButton = $(e.relatedTarget);
+        var handTarget = '#' + invokingButton.attr('data-hand');
+        $('#chooseCharacterBody .btn').click(function () {
+            theModal.modal('hide');
+            var removedCard = crewCards.removeCard($(this).html());
+            $(handTarget).append("<button style='margin:7px 15px 17px 0;' type='button' class='btn btn-success' data-toggle='popover' data-trigger='hover' data-placement='top' data-content='"+removedCard.desc+"'>"+removedCard.name+"</button>");
+            $('[data-toggle="popover"]').popover({html:true});
+            updateRecRoom();
+            invokingButton.prop('disabled', true);
+        });
+    });
+    
+    $('#chooseCharModal').on('hidden.bs.modal', function () {
+        $("#chooseCharacterBody").empty();
+    });
+    
+    $('#dealStartingHandsButton').click(function () {
+        dealStartingHands();
+        $(this).prop('disabled', true);
+    });
+    
     loadFileAsJSONmodel("/DeckSim/decks.json");
     
     player1Hand = new Hand("Player 1 Hand");
     player2Hand = new Hand("Player 2 Hand");
 });
 
-$('#drawModal').on('show.bs.modal', function (event) {
-    $("#deckButtons").empty();
+
+
+
+
+function getDeckFromName( name ) {
     for(var i=0; i < allDecks.length; i++) {
-        $("#deckButtons").append(" \
-            <div class='radio'> \
-              <label><input type='radio' name='optradio'>"+ allDecks[i].name + "</label> \
-            </div> \
-        ");
+        if(allDecks[i].name === name) {
+            return allDecks[i];
+        }
     }
-});
+}
 
-$('#removeCardAcceptButton').click(function () {
-    var cardID = document.getElementById("cardIDinput").value;
-    console.log("removecard() " + cardID);
-});
-
-$('#chooseCharModal').on('show.bs.modal', function (e) {
-    for(var i=0; i < crewCards.length(); i++) {
-        $("#chooseCharacterBody").append("<button style='margin:7px 15px 17px 0;' type='button' class='btn btn-success' data-toggle='popover' data-trigger='hover' data-placement='top' data-content='"+crewCards.get(i).desc+"'>"+crewCards.get(i).name+"</button>");
+function getHandFromHTMLid( searchHTMLid ) {
+    for(var i=0; i < allHands.length; i++) {
+        if(allHands[i].HTMLid === searchHTMLid) {
+            return allHands[i];
+        }
     }
-    var theModal = $(this);
-    var invokingButton = $(e.relatedTarget);
-    var handTarget = '#' + invokingButton.attr('data-hand');
-    $('#chooseCharacterBody .btn').click(function () {
-        theModal.modal('hide');
-        var removedCard = crewCards.removeCard($(this).html());
-        $(handTarget).append("<button style='margin:7px 15px 17px 0;' type='button' class='btn btn-success' data-toggle='popover' data-trigger='hover' data-placement='top' data-content='"+removedCard.desc+"'>"+removedCard.name+"</button>");
-        $('[data-toggle="popover"]').popover({html:true});
-        updateRecRoom();
-        invokingButton.prop('disabled', true);
-    });
-});
-
-$('#chooseCharModal').on('hidden.bs.modal', function () {
-    $("#chooseCharacterBody").empty();
-});
-
-$('#dealStartingHandsButton').click(function () {
-    dealStartingHands();
-    $(this).prop('disabled', true);
-});
+}
 
 function loadFileAsJSONmodel(URL) {
     $.getJSON(URL) 
@@ -94,16 +131,25 @@ function loadFileAsJSONmodel(URL) {
             t2.standardInit();
             t3 = new StandardDeck("The Thing-Deck #3", "ThingDeck3", json.ThingDeck3.BaseCards);
             t3.standardInit();
-            destruction = new StandardDeck("Destruction Deck", "destruction", json.Destruction);
+            destruction = new StandardDeck("Destruction Deck", "destruction", json.Destruction.BaseCards);
             destruction.standardInit();
             
             allDecks = [o1,o2,o3,t1,t2,t3, destruction];
+            
+            player1Hand = new Hand("Player 1 Hand", "player1Hand" );
+            console.log("player1Hand: " + player1Hand.HTMLid);
+            player2Hand = new Hand("Player 2 Hand", "player2Hand" );
+            console.log("player2Hand: " + player2Hand.HTMLid);
+            thingHand = new Hand("The Thing Hand", "thingHand" );
+            console.log("thingHand: " + thingHand.HTMLid);
+            
+            allHands = [player1Hand, player2Hand, thingHand];
             
             crewCards = new Deck("Crew Cards", "crew-cards", json.CrewCards);
             crewCards.init();
             updateRecRoom();
             
-            tasks = new Deck("Tasks", "tasks", json.TaskCards);
+            tasks = new Deck("Tasks", "tasks", json.TaskCards.BaseCards);
             tasks.init();
             for(var i=0; i < tasks.length(); i++) {
                 $("#tasks").append("<button style='margin:7px 15px 17px 0;' type='button' class='btn btn-info' data-toggle='popover' data-trigger='hover' data-placement='top' data-content='"+tasks.get(i).desc+"'>"+tasks.get(i).name+"</button>");
@@ -126,9 +172,9 @@ function loadFileAsJSONmodel(URL) {
 }
 
 function dealStartingHands() {
-    var player1 = new Hand("Player 1 Hand", o1, STARTING_HAND_SIZE, "p1hand" );
-    var player2 = new Hand("Player 2 Hand", o1, STARTING_HAND_SIZE, "p2hand" );
-    var thing = new Hand("The Thing Hand", t1, STARTING_HAND_SIZE, "thingHand" );
+    player1Hand.draw(o1, STARTING_HAND_SIZE);
+    player2Hand.draw(o1, STARTING_HAND_SIZE);
+    thingHand.draw(t1, STARTING_HAND_SIZE);
 }
 
 function updateRecRoom() {
@@ -202,7 +248,7 @@ function Deck( inName, HTMLelementID, cardList ) {
     // or else there will not be a cardList to tie to;
     this.init = function() {
         this.stack = this.tieToCardList(cardList);
-        console.log(this.name +"'s stack: " + this.stack);
+        //console.log(this.name +"'s stack: " + this.stack);
         this.shuffle();
     };
     
@@ -210,14 +256,27 @@ function Deck( inName, HTMLelementID, cardList ) {
         console.log("parent function");
     };
     
-    this.removeCard = function( cardName ) {
-        console.log("removing " + cardName);
-        for(var i = 0; i < this.stack.length ; i++) {
-            if(this.stack[i].name === cardName) {
-                var removedCard = this.stack[i];
+    
+    this.removeCard = function( cardSearchTerm ) {
+        //console.log("search term: " + cardSearchTerm);
+        var removedCard = '';
+        var found = false;
+        for(var i = 0; i < this.stack.length && found === false ; i++) {
+            //console.log("searching against " + this.stack[i].id + " and " + this.stack[i].name);
+            if(this.stack[i].id == cardSearchTerm || 
+               this.stack[i].name === cardSearchTerm ) {
+                removedCard = this.stack[i];
+                found = true;
                 this.stack.splice(i,1);
-                return removedCard;
             }
+        }
+        //console.log('checking removedCard: ' + removedCard);
+        if(found === true) {
+            //console.log("returning...");
+            return removedCard;
+        }
+        else {
+            alert("card not found");
         }
     };
 }
@@ -250,29 +309,47 @@ function StandardDeck(inName, HTMLelementID, cardList){
     };
 }
 
-function Hand( PlayerName, deck, startingHandSize, HTMLelementID ) {
+function Hand( PlayerName, HTMLelementID ) {
     var contents = new Array();
     var pName = PlayerName;
     this.HTMLid = HTMLelementID;
-
-    //Priveleged
+    
+    
     this.drawOne = function( deck ) {
-        contents.push(deck.pop());
-        this.updateDisplay();
-        deck.updateDisplay();
+        var card = deck.pop();
+        contents.push(card);
+        this.appendCardText(card);
     };
     
-    this.drawSpecificCard = function( deck , cardName ) {
-        contents.push(deck.removeCard(cardName));
+    this.appendCardText = function( card ) {
+        $('#' + this.HTMLid).append("<span>");
+        $('#' + this.HTMLid).append( card.getPlaceUnderButtonHTML() );
+        $('#' + this.HTMLid).append( card.toString() );
+        $('#' + this.HTMLid).append("</span>");
     };
-
+    
+    this.drawSpecificCard = function( deck , cardNum ) {
+        if(cardNum === "") {
+            console.log("empty draw!");
+            this.drawOne(deck);
+            return;
+        }
+        else {
+            //console.log("deck: " + deck + ",cardNum:" + cardNum);
+            var drawnCard = deck.removeCard(cardNum);
+            contents.push( drawnCard );
+            //console.log("appending: " + drawnCard.toString() + " to " + "$(#" + this.HTMLid + ")");
+            this.appendCardText( drawnCard );
+        }
+    };
+    
     // Returns an array of cards from the top of 'deck' 
     this.draw = function( deck, numCards ) {
         for(var i = 0; i < numCards; i++) {
             this.drawOne(deck);
         }
     };
-
+    
     this.toString = function() {
         var string = ""; // = "<b>" + pName + ": </b> <p>";
         for(var i = 0; i < contents.length; i++) {
@@ -286,14 +363,24 @@ function Hand( PlayerName, deck, startingHandSize, HTMLelementID ) {
     this.updateDisplay = function() {
         document.getElementById(this.HTMLid).innerHTML = this.toString();
     };
-
-    if(deck !== null) {
-        this.draw(deck, startingHandSize);
-    }
 }
 
 function Card(inID, inName, inDesc ) {
     this.id = inID;
     this.name = inName;
     this.desc = inDesc;
+    
+    //divID should refer to the id of the div which contains the card text
+    this.getPlaceUnderButtonHTML = function( ) {
+        return "<button \n\
+                 style='margin:3px 3px 3px 0;' \n\
+                 type='button' \n\
+                 class='btn btn-default' \n\
+                 data-toggle='modal' \n\
+                 data-target='#placeUnderModal'>Place Under Card</button>";
+    };
+    
+    this.toString = function() {
+        return "<strong>" + this.name + "</strong><i><small>"+this.desc+ "(" + this.id + ")</small></i><p>";
+    };
 }
